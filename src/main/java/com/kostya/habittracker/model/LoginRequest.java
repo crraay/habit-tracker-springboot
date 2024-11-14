470fa51d1a00ee2911a2493fab49034874249f4a
Kostya <crraay@gmail.com>
2024-11-14 17:08:15 +0700

    feat: mask password in LoginRequest for improved security in logging

--- src/main/java/com/kostya/habittracker/model/LoginRequest.java
+++ src/main/java/com/kostya/habittracker/model/LoginRequest.java
@@ -1,11 +1,16 @@
 package com.kostya.habittracker.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 public class LoginRequest {
 
     String username;
 
+    @ToString.Exclude
     String password;
+
+    @ToString.Include(name = "password")
+    private String passwordMasked() { return password == null ? null : "*****"; }
 }
