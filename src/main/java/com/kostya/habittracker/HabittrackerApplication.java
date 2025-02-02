3dd5d84cddaa265c1121e18e2aa2ed7b48a52a3c
Kostya <crraay@gmail.com>
2025-02-02 12:59:43 +0700

    refactor: rename HabittrackerApplication to HabitTrackerApplication for consistency

--- src/main/java/com/kostya/habittracker/HabittrackerApplication.java
+++ src/main/java/com/kostya/habittracker/HabittrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HabittrackerApplication {
+public class HabitTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HabittrackerApplication.class, args);
+		SpringApplication.run(HabitTrackerApplication.class, args);
 	}
 
 }
