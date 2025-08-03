2153791e7b7c943405d7a7e3cc9604fa2dd9db7a
Kostya <crraay@gmail.com>
2025-08-03 11:23:59 +0700

    feat: add logging for application startup in HabitTrackerApplication

--- src/main/java/com/kostya/habittracker/HabitTrackerApplication.java
+++ src/main/java/com/kostya/habittracker/HabitTrackerApplication.java
@@ -3,10 +3,14 @@ package com.kostya.habittracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class HabitTrackerApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting Habit Tracker application.");
 		SpringApplication.run(HabitTrackerApplication.class, args);
 	}
 
