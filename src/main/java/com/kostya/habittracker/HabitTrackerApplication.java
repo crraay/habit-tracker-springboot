47432156f99aac1773f1f5f72256823868091e16
Kostya <crraay@gmail.com>
2025-08-03 15:08:51 +0700

    chore: update JAR file paths in GitHub Actions workflows and remove unnecessary logging

--- src/main/java/com/kostya/habittracker/HabitTrackerApplication.java
+++ src/main/java/com/kostya/habittracker/HabitTrackerApplication.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 public class HabitTrackerApplication {
 
 	public static void main(String[] args) {
-		log.info("Starting Habit Tracker application.");
 		SpringApplication.run(HabitTrackerApplication.class, args);
 	}
 
