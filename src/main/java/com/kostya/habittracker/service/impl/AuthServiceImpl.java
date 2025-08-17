f97a2b261e8518fc3f1c894e5ef59b1a4262ddfa
Kostya <crraay@gmail.com>
2025-08-17 15:19:56 +0700

    feat: enhance AuthServiceImpl to use secure key generation for JWT signing

--- src/main/java/com/kostya/habittracker/service/impl/AuthServiceImpl.java
+++ src/main/java/com/kostya/habittracker/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kostya.habittracker.service.impl;
 
+import java.security.Key;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ import com.kostya.habittracker.service.AuthService;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -39,11 +42,12 @@ public class AuthServiceImpl implements AuthService {
             throw new UnauthorizedException("Invalid username or password"); 
         }
       
+        Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(this.SECRET_KEY)); 
         String token = Jwts.builder()
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + this.TOKEN_EXPIRES_IN))
-                .signWith(SignatureAlgorithm.HS256, this.SECRET_KEY)
+                .subject(user.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + this.TOKEN_EXPIRES_IN))
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
         
         return new LoginResponse(token, this.TOKEN_EXPIRES_IN);
